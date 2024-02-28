7034bf3e7e7c8d33b674a4b8870264a4a679c71a
Zakpm <113630793+Zakpm@users.noreply.github.com>
2024-02-28 16:26:50 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,20 +1,20 @@
 export const environmentAuth = {
   production: true,
-  apiUrl: 'http://securityload-643409079.eu-west-3.elb.amazonaws.com/auth'
+  apiUrl: ''
 };
 
 export const environmentCreation = {
   production: true,
-  apiUrl: 'http://creationload-703266574.eu-west-3.elb.amazonaws.com'
+  apiUrl: ''
 };
 
 export const environmentContact = {
   production: true,
-  apiUrl: 'http://contactload-646292781.eu-west-3.elb.amazonaws.com'
+  apiUrl: ''
 };
 
 export const environmentResetPassword = {
   production: true,
-  apiUrl: 'http://securityload-643409079.eu-west-3.elb.amazonaws.com/reset-password'
+  apiUrl: ''
 };
 
